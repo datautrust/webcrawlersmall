23e208761a4baf58c6bbbb3feaef48b6e0598f0d
astroboy <datautrust@yahoo.com>
2023-02-07 14:02:10 -0800

    crawl site with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,28 @@
 const {JSDOM} = require('jsdom')
+async function crawlPage(currentURL) {
+    console.log("actively crawling ", `${currentURL}`)
+    try{
+       const resp = await fetch(currentURL)
+       if (resp.status > 399) {
+        console.log(`error in fetch with status code ${resp.status}
+        on pagexxx: ${currentURL}`)
+        return
+       }
+//check for valid html content by looking at the header
+       const contentType = resp.headers.get("content-type")
+       if (!contentType.includes("text/html")) {
+        console.log("non html response, content type",
+        `${contentType}`," on page ",`${currentURL}`)
+        return
+       }
 
+       console.log(await resp.text())  //earlier in labs we used .json but here we just want the html text
+    } catch (err){
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
+    }
+
+   
+}
 function getURLsFromHTML(htmlBody, baseURL){
 //htmlbody = the html string
 //baseURL what we are crawling
@@ -50,5 +73,6 @@ function normalizeURL(urlString){
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage,
 }
--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("hello my world")
+const {crawlPage} = require('./crawl.js')
+
+function main() {
+    console.log("hello my world")
+    for (const arg of process.argv) {
+        console.log("arguments =",arg)
+    }
+    if (process.argv.length < 3){
+        console.log("no website provided") 
+        process.exit(1);
+    }
+    if (process.argv.length > 3){
+        console.log("too many command line arguments") 
+        process.exit(1);
+    }
+    const baseURL = process.argv[2];
+    console.log("starting crawl of ", `${baseURL}`)
+    crawlPage(baseURL)
+}
+main()
