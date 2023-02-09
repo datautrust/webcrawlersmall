39ed593bb944ba8af72082f56a047789cb5d72c0
astroboy <datautrust@yahoo.com>
2023-02-08 17:25:25 -0800

    crawl page recursively working

--- crawl.js
+++ crawl.js
@@ -1,26 +1,53 @@
 const {JSDOM} = require('jsdom')
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL,currentURL,pages) {
+   
+// lets make sure we are crawling pages from 1 site, not different sites
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    console.log("pages ",pages)
+    console.log("baseURLObj", baseURLObj)
+    console.log("curentURLObj ", currentURLObj)
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    } 
+    // now lets check if we already crawled this page
+    //we do this by checking this currentURL exists in the pages object
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if (pages[normalizedCurrentURL] > 0){
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+//init pages
+    pages[normalizedCurrentURL] = 1
     console.log("actively crawling ", `${currentURL}`)
+
     try{
        const resp = await fetch(currentURL)
        if (resp.status > 399) {
         console.log(`error in fetch with status code ${resp.status}
         on pagexxx: ${currentURL}`)
-        return
+        return pages
        }
 //check for valid html content by looking at the header
        const contentType = resp.headers.get("content-type")
        if (!contentType.includes("text/html")) {
         console.log("non html response, content type",
         `${contentType}`," on page ",`${currentURL}`)
-        return
+        return pages
        }
+//console.log(await resp.text())  //earlier in labs we used .json but here we just want the html text
+       const htmlBody = await resp.text();
+//now we traverse & extract all the links 
+       const nextURLs = getURLsFromHTML(htmlBody,baseURL)
+       for (const nextURL of nextURLs) {
+        pages = await crawlPage(baseURL, nextURL,pages)
 
-       console.log(await resp.text())  //earlier in labs we used .json but here we just want the html text
+       }
+       
     } catch (err){
         console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
     }
-
+    return pages;
    
 }
 function getURLsFromHTML(htmlBody, baseURL){
