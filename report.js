d36780356aecd1fcab70f09c9ae7b3bdeae661e4
astroboy <datautrust@yahoo.com>
2023-02-08 21:03:22 -0800

    Webcrawl writes data to csv file

--- report.js
+++ report.js
@@ -1,13 +1,29 @@
+const fs = require('fs')
 function printReport(pages) {
     console.log("=============================================")
     console.log("REPORT")
     console.log("=============================================")
     const sortedPages = sortPages(pages)
+    let csvContent =''
     for (const sortedPage of sortedPages) {
         const url = sortedPage[0]
         const hits = sortedPage[1]
-        console.log(`Found ${hits} liks to page: ${url}`)
+        console.log(`Found ${hits} links to page: ${url}`)
+        csvContent += `Found ${hits} links to page: ${url}`+ '\n'
+        
+                
     }
+    fs.writeFile('output.csv', csvContent, 'utf8', function (err) {
+        if (err) {
+          console.log('An error occurred while writing the CSV file:', err);
+        } else {
+            
+          console.log('The CSV file was written successfully.');
+        }
+      });
+      
+
+
     console.log("=============================================")
     console.log("END REPORT")
     console.log("=============================================")
